73a288fc9d1298cb3666e7626f77a0a2547751db
Inaue <cafe.ina@outlook.com>
2022-05-23 00:02:34 +0000

    atualizacao no arquivo serie.sh

--- 5pratica/serie.sh
+++ 5pratica/serie.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-tr=50
+tr=$1
 soma=0
+fat=1
+den=0
+sin=-1
 
-for t in $((seq 1 1 $tr))
+for tri in $(seq $tr)
 do
-	echo $t
+	fat=$[fat*tri]
+	den=$[(2*den)+1] # den = f(tri) = (2^tri) - 1
+	sin=$[-sin]
+	fra=$(echo $fat/$den | bc -l)
+	fra=$(echo $sin*$fra | bc -l)
+#	soma=$(echo soma+(sin*fra) | bc -l)
+	echo $fra
 done
 
-echo A soma dos $tr termos da sequencia eh $soma
-unset tr soma
+echo A soma dos $tri termos da sequencia eh $soma
+unset tri tr soma fat den sin fra
 
