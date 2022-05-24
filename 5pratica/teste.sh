87605fe31b9e9106dfe327599729c6b2ddcda694
Inaue <cafe.ina@outlook.com>
2022-05-24 23:26:28 +0000

    deletando teste.sh desnecessario

--- 5pratica/teste.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Digite a medida dos 3 lados do paralelepipedo:
-read a b c
-diagonal=$[(a**2)+(b**2)+(c**2)]
-diagonal=$(echo "sqrt($diagonal)" | bc -l)
-echo A diagonal do paralelepipedo mede $diagonal
-unset a b c diagonal
-
