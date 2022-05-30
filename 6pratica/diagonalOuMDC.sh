05885a210e3125418d4ca0332a97768181de5cf3
Inaue <cafe.ina@outlook.com>
2022-05-30 16:15:00 +0000

    Updating file.

--- 6pratica/diagonalOuMDC.sh
+++ 6pratica/diagonalOuMDC.sh
@@ -6,27 +6,42 @@ c=0
 diagonal=0
 calculo=""
 mdc=0
+resto=0
+num1=0
+num2=0
 
 function diagLelepi ()
 {
-	echo Digite a medida dos 3 lados do paralelepipedo:
-	read a b c
-	diagonal=$[(a**2)+(b**2)+(c**2)]
+	diagonal=$[($1**2)+($2**2)+($3**2)]
 	diagonal=$(echo "sqrt($diagonal)" | bc -l)
-	echo A diagonal do paralelepipedo mede $diagonal
 }
 
 function mdc2Num ()
 {
-	mdc=$[(a**2)+(b**2)+(c**2)]
-	mdc=$(echo "sqrt($diagonal)" | bc -l)
+	num1=$1
+	num2=$2
+	resto=$[$1%$2]
+	while [ $resto -ne 1 ]
+	do
+		num1=$num2
+		num2=$resto
+		resto=$[$num1%$num2]
+	done
+	mdc=resto
 }
 
 select calculo in "Diagonal do Paralelepipedo" "MDC de 3 Numeros" "Sair"
 do
 	case $calculo in
-		"Diagonal do Paralelepipedo") diagLelepi;;
-		"MDC de 3 Numeros") echo t && echo a;;
+		"Diagonal do Paralelepipedo") 	echo Digite a medida dos 3 lados do paralelepipedo: &&
+						read a b c &&
+						diagLelepi a b c &&
+						echo A diagonal do paralelepipedo mede $diagonal;;
+		"MDC de 3 Numeros") 	echo Digite os 3 numeros cujo MDC deseja obter: &&
+					read a b c &&
+					mdc2Num a b &&
+					mdc2Num mdc c
+					echo O MDC dos numeros eh $mdc;;
 		"Sair") break;;
 		*) echo ERRO: entrada invalida!
 	esac
